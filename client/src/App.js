6d9aed2fd5b8654c20100119c3c943fc527b0c2f
Mark Liu <markliu202@gmail.com>
2021-01-16 20:30:48 -0500

    Speech recognition

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import Income from "./Income/Income";
+import SpeechToText from "./STT/SpeechToText";
 import "./App.css";
 
 function App() {
   return (
     <div className="App-header">
       <Income />
+      <SpeechToText />
     </div>
   );
 }

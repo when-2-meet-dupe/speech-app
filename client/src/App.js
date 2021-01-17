3076f4b443e94082e07a7a69b60d1116e0f2f0f9
George Liu <georgeliu220@gmail.com>
2021-01-16 20:37:32 -0500

    add delete post route

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
 import Income from "./Income/Income";
-<<<<<<< HEAD
 import SpeechToText from "./STT/SpeechToText";
-=======
 import Nav from "./global/Nav";
->>>>>>> 3726934911367a5889b524e8a322b53df7f45349
 import "./App.css";
 
 function App() {

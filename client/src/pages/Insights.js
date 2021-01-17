b060888ac14ccd45053d4965cffb48d827b0b1b3
Helen Gezahegn <gezahegn@ualberta.ca>
2021-01-17 04:25:34 -0700

    feat: insights charts

--- client/src/pages/Insights.js
+++ client/src/pages/Insights.js
@@ -1,131 +1,145 @@
 import React, { useState } from "react";
-import "../App.css";
-import Chart from "react-google-charts";
+import { Box } from "@material-ui/core";
 import { useSelector } from "react-redux";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { getTransactions } from "../actions/transactions";
+import { PieArcSeries, PieChart } from "reaviz";
 
 function Insights() {
   const transactions = useSelector((state) => state.transactions);
-  const dispatch = useDispatch();
 
-  const getExpenseSums = (transactions) => {
+  const getExpenseSums = () => {
     let expenseSums = {
       housing: 0,
       food: 0,
       clothing: 0,
       business: 0,
-      transportation: 0,
+      transportation: 0
     };
 
     for (let i = 0; i < transactions.length; i++) {
-      if (transactions[0].category === "housing") {
-        expenseSums.housing++;
-      } else if (transactions[0].category === "food") {
-        expenseSums.food++;
-      } else if (transactions[0].category === "clothing") {
-        expenseSums.clothing++;
-      } else if (transactions[0].category === "business") {
-        expenseSums.business++;
-      } else if (transactions[0].category === "transportation") {
-        expenseSums.business++;
+      if (transactions[i].category === "housing") {
+        expenseSums.housing += transactions[i].amount;
+      } else if (transactions[i].category === "food") {
+        expenseSums.food += transactions[i].amount;
+      } else if (transactions[i].category === "clothing") {
+        expenseSums.clothing += transactions[i].amount;
+      } else if (transactions[i].category === "business") {
+        expenseSums.business += transactions[i].amount;
+      } else if (transactions[i].category === "transportation") {
+        expenseSums.transportation += transactions[i].amount;
       }
     }
-
-    return incomeSums;
+    return expenseSums;
   };
 
-  const getIncomeSums = (transactions) => {
+  const getIncomeSums = () => {
     let incomeSums = {
       investment: 0,
       earnings: 0,
       deposit: 0,
-      etransfer: 0,
+      etransfer: 0
     };
 
     for (let i = 0; i < transactions.length; i++) {
-      if (transactions[0].category === "investment") {
-        incomeSums.investment++;
-      } else if (transactions[0].category === "earnings") {
-        incomeSums.earnings++;
-      } else if (transactions[0].category === "deposit") {
-        incomeSums.deposit++;
-      } else if (transactions[0].category === "etransfer") {
-        incomeSums.etransfer++;
+      if (transactions[i].category === "investment") {
+        incomeSums.investment += transactions[i].amount;
+      } else if (transactions[i].category === "earnings") {
+        incomeSums.earnings += transactions[i].amount;
+      } else if (transactions[i].category === "deposit") {
+        incomeSums.deposit += transactions[i].amount;
+      } else if (transactions[i].category === "etransfer") {
+        incomeSums.etransfer += transactions[i].amount;
       }
     }
 
     return incomeSums;
   };
 
-  const getTypeSums = (transactions) => {
+  const getTypeSums = () => {
     let typeSums = {
       expense: 0,
-      income: 0,
+      income: 0
     };
 
     for (let i = 0; i < transactions.length; i++) {
-      if (transactions[0].type === "expense") {
-        typeSums.expense++;
-      } else if (transactions[0].type === "income") {
-        typeSums.income++;
+      if (transactions[i].type === "expense") {
+        typeSums.expense += transactions[i].amount;
+      } else if (transactions[i].type === "income") {
+        typeSums.income += transactions[i].amount;
       }
     }
 
     return typeSums;
   };
 
-  const [incomeSums, setIncomeSums] = useState({
-    investment: 0,
-    earnings: 0,
-    deposit: 0,
-    etransfer: 0,
-  });
+  const [incomeSums, setIncomeSums] = useState(getIncomeSums);
+  const [expenseSums, setExpenseSums] = useState(getExpenseSums);
+  const [typeSums, setTypeSums] = useState(getTypeSums);
 
-  const [expenseSums, setExpenseSums] = useState({
-    housing: 0,
-    food: 0,
-    clothing: 0,
-    business: 0,
-    transportation: 0,
-  });
+  const renderExpenses = () => {
+    return (
+      <Box flexDirection="column" alignContent="center" className="leftChart">
+        <h4>Expense</h4>
+        <PieChart
+          label="Expense"
+          height={300}
+          width={400}
+          data={[
+            { key: "Housing", data: expenseSums.housing },
+            { key: "Food", data: expenseSums.food },
+            { key: "Clothing", data: expenseSums.clothing },
+            { key: "Business", data: expenseSums.business },
+            { key: "Transportation", data: expenseSums.transportation }
+          ]}
+          series={<PieArcSeries colorScheme="Accent" />}
+        />
+      </Box>
+    );
+  };
 
-  const [typeSums, setTypeSums] = useState({
-    expense: 0,
-    income: 0,
-  });
+  const renderIncome = () => {
+    return (
+      <Box flexDirection="column">
+        <h4>Income</h4>
+        <PieChart
+          label="Income"
+          colorScheme="greens"
+          height={300}
+          width={400}
+          data={[
+            { key: "Investments", data: incomeSums.investment },
+            { key: "Earnings", data: incomeSums.earnings },
+            { key: "Deposit", data: incomeSums.deposit },
+            { key: "Etransfer", data: incomeSums.etransfer }
+          ]}
+          series={<PieArcSeries colorScheme="Accent" />}
+        />
+      </Box>
+    );
+  };
 
-  setTypeSums(getTypeSums(transactions));
-  setIncomeSums(getIncomeSums(transactions));
-  setExpenseSums(getExpenseSums(transactions));
-
-  // useEffect(() => {
-  //   dispatch(getTransactions());
-  //   setTypeSums(getTypeSums(transactions));
-  //   setIncomeSums(getIncomeSums(transactions));
-  //   setExpenseSums(getExpenseSums(transactions));
-  // }, []);
+  const renderTypes = () => {
+    return (
+      <>
+        <PieChart
+          height={400}
+          width={400}
+          data={[
+            { key: "Income", data: typeSums.income },
+            { key: "Expense", data: typeSums.expense }
+          ]}
+          series={<PieArcSeries doughnut={true} colorScheme="Accent" />}
+        />
+      </>
+    );
+  };
 
   return (
     <>
-      <Chart
-        width={"500px"}
-        height={"300px"}
-        chartType="PieChart"
-        loader={<div>Expense Categories</div>}
-        data={[
-          ["housing", expenseSums.housing],
-          ["food", expenseSums.food],
-          ["clothing", expenseSums.clothing],
-          ["business", expenseSums.business],
-          ["transportation", expenseSums.transportation],
-        ]}
-        options={{
-          title: "My Daily Activities",
-        }}
-        rootProps={{ "data-testid": "1" }}
-      />
+      <Box display="flex" flexDirection="row" textAlign="center">
+        {renderExpenses()}
+        {renderIncome()}
+      </Box>
+      {renderTypes()}
     </>
   );
 }

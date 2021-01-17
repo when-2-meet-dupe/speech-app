c7e55c030989f4b23e43ebaea8ff8520142c7ffc
Mark Liu <markliu202@gmail.com>
2021-01-17 00:28:53 -0500

    Moved table

--- client/src/pages/Insights.js
+++ client/src/pages/Insights.js
@@ -1,16 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "../App.css";
 
 export default function Insights() {
-
   // configure these lists with database info
-  let incomesList = ["Housing", "$1","Food","$","Clothing","$1", "Business", "$4"]
-  let expensesList = ["Investment", "$1","Salary","$","Deposit","$1", "Business", "$4"]
+  let incomesList = [
+    "Housing",
+    "$1",
+    "Food",
+    "$",
+    "Clothing",
+    "$1",
+    "Business",
+    "$4",
+  ];
+  let expensesList = [
+    "Investment",
+    "$1",
+    "Salary",
+    "$",
+    "Deposit",
+    "$1",
+    "Business",
+    "$4",
+  ];
 
-  const [incomes, setIncomes] = useState(incomesList)
-  const [expenses, setExpenses] = useState(expensesList)
+  const [incomes, setIncomes] = useState(incomesList);
+  const [expenses, setExpenses] = useState(expensesList);
 
-  const [tempNum, setTempNum] = useState(0)
+  const [tempNum, setTempNum] = useState(0);
 
   // const renderIncomes = incomes.map((income) => {
   //   console.log(incomes)
@@ -33,58 +50,9 @@ export default function Insights() {
 
   // })
 
-
   return (
     <div className="App-header">
-      <h1 className="insightsTitle">Insights</h1>
-      <div className="cards">
-        <div className="card1">
-          <h1>Income</h1>
-          <table>
-            <tr>
-              <th>Category</th>
-              <th>Amount</th>
-            </tr>
-            <tr>
-              <td>Housing</td>
-              <td>$1</td>
-            </tr>
-            <tr>
-              <td>Food</td>
-              <td>$2</td>
-            </tr>
-            <tr>
-              <td>Clothing</td>
-              <td>$3</td>
-            </tr>
-            <tr>
-              <td>Business</td>
-              <td>$4</td>
-            </tr>
-          </table>
-        </div>
-        <div className="card2">
-          <h1>Expenses</h1>
-          <table>
-            <tr>
-              <th>Category</th>
-              <th>Amount</th>
-            </tr>
-            <tr>
-              <td>Investment</td>
-              <td>$1</td>
-            </tr>
-            <tr>
-              <td>Salary</td>
-              <td>$2</td>
-            </tr>
-            <tr>
-              <td>Deposit</td>
-              <td>$3</td>
-            </tr>
-          </table>
-          </div>
-      </div>
+      <p>yo</p>
     </div>
   );
 }
--- client/src/pages/Transactions.js
+++ client/src/pages/Transactions.js
@@ -3,7 +3,55 @@ import "../App.css";
 export default function Transactions() {
   return (
     <div className="App-header">
-      <div>Transactions</div>
+      <h1 className="insightsTitle">Insights</h1>
+      <div className="cards">
+        <div className="card1">
+          <h1>Income</h1>
+          <table>
+            <tr>
+              <th>Category</th>
+              <th>Amount</th>
+            </tr>
+            <tr>
+              <td>Housing</td>
+              <td>$1</td>
+            </tr>
+            <tr>
+              <td>Food</td>
+              <td>$2</td>
+            </tr>
+            <tr>
+              <td>Clothing</td>
+              <td>$3</td>
+            </tr>
+            <tr>
+              <td>Business</td>
+              <td>$4</td>
+            </tr>
+          </table>
+        </div>
+        <div className="card2">
+          <h1>Expenses</h1>
+          <table>
+            <tr>
+              <th>Category</th>
+              <th>Amount</th>
+            </tr>
+            <tr>
+              <td>Investment</td>
+              <td>$1</td>
+            </tr>
+            <tr>
+              <td>Salary</td>
+              <td>$2</td>
+            </tr>
+            <tr>
+              <td>Deposit</td>
+              <td>$3</td>
+            </tr>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
